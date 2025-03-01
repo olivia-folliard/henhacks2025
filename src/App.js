64c9dcd0c36e60dabe18af36d528422da8de4c52
OLIVIA FOLIIARD <FOLLIARD@UDEL.EDU>
2025-03-01 18:56:41 -0500

    welcome component added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="/Home" element={<Home />} />
         </Routes> 
       </div>
     </Router>
