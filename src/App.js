7587e2d14349e88c2ed0ddba6cc84348840b0755
OLIVIA FOLIIARD <FOLLIARD@UDEL.EDU>
2025-03-01 16:41:01 -0500

    deployment working :)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Routes>
           <Route path="/" element={<Home />} />
+        </Routes> 
       </div>
     </Router>
   );  
