4fd19dbaf22f661f405b3f28b75024381dfed49f
OLIVIA FOLIIARD <FOLLIARD@UDEL.EDU>
2025-03-01 16:25:54 -0500

    switched to hash router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
           <Route path="/" element={<Home />} />
-        </Routes>
       </div>
     </Router>
   );  
