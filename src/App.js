e275c9cb12b694657e6c61d69cb3692992dfd083
OLIVIA FOLIIARD <FOLLIARD@UDEL.EDU>
2025-03-01 22:50:35 -0500

    welcome page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from "react";
   import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Survey from "./components/Survey";
+import Welcome from "./components/Welcome";
 
 function App() {
+
   return (
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/survey" element={<Survey />} />
         </Routes>
       </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Welcome() {
+
+    const [selected, setSelected] = useState([]);
+
+    const handleCheckbox = () => {
+        setSelected();
+    }
+
+    const checkboxes = [
+        'I feel great!', 
+        'A little tired', 
+        'Sore or achy', 
+        'Feeling under the weather',
+        'Stressed or overwhelmed'
+    ]
+
     return(
-        <h1>Hey Name, glad you're here! Let's check in: <br />
-        how are you feeling today?</h1>
+        <>
+            <h1>Hey Name, glad you're here! Let's check in: <br />
+            how are you feeling today?</h1>
+
+        <div>
+            <input 
+                type="range"
+                min="1"
+                max="5"
+            />
+        </div>
+
+            {checkboxes.map(checkbox => (
+                <label key={checkbox}>
+                <input 
+                    type="checkbox" 
+                    value={checkbox}
+                    onChange={handleCheckbox}
+                    checked={selected.includes(checkbox)}
+                /> {checkbox}
+            </label>
+            ))}
+        </>
     )
 }
 
